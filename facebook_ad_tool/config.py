7835dbd2635e01cb95208050a641d4dfc225ba10
Darcy Brás da Silva <stardevelin@gmail.com>
2017-08-04 18:55:13 +0100

    use a default key so that testing works out of the box the implementation requires an environment variable. ideally this gets eventually documented so that a user can also not depend on env variables to changes the secret key

--- facebook_ad_tool/config.py
+++ facebook_ad_tool/config.py
@@ -3,5 +3,5 @@ import os
 class DevelopmentConfig(object):
     SQLALCHEMY_DATABASE_URI = "postgresql://ubuntu:thinkful@localhost:5432/ad_tool"
     DEBUG = True
-    SECRET_KEY = os.environ.get("AD_TOOL_SECRET_KEY", "")
+    SECRET_KEY = os.environ.get("AD_TOOL_SECRET_KEY", "nothing")
     
